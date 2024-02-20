65405e38236208f367ea2ce88696f25505661f2f
marcelogattermann <marcelo_gattermann@hotmail.com>
2024-02-20 19:52:46 -0300

    20-02

---
+++ cadastros.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+include 'cabecalho.php';
+?>
+
+<body>
+    <input type="text" placeholder="Nome do usuario" id="nome_usuario">
+    <input type="number" placeholder="CPF" id="cpf">
+    <input type="date" placeholder="Data de nascimento" id="data_nascimento">
+    <input type="number" placeholder="Telefone" id="telefone">
+    <input type="text" placeholder="Endereço" id="endereco">
+    <input type="number" placeholder="Numero da casa" id="num_casa">
+    <input type="text" placeholder="Bairro" id="bairro">
+    <input type="text" placeholder="Cidade" id="fk_id_tb_cidades">
+    <input type="text" placeholder="Tipo do usuario" id="tipo_usuario">
+    <input type="text" placeholder="Login" id="login">
+    <input type="passoword" placeholder="Senha" id="senha">
+    <button onclick="cadastrar()" type="submit">Entrar</button>
+</body>
+<?php
+include 'rodape.php';
+?>
+
+</html>
+<script>
+    function cadastrar() {
+        let nome_usuario = document.getElementById('nome_usuario').value;
+        let cpf = document.getElementById('cpf').value;
+        let data_nascimento = document.getElementById('data_nascimento').value;
+        let telefone = document.getElementById('telefone').value;
+        let endereco = document.getElementById('endereco').value;
+        let num_casa = document.getElementById('num_casa').value;
+        let bairro = document.getElementById('bairro').value;
+        let fk_id_tb_cidades = document.getElementById('fk_id_tb_cidades').value;
+        let tipo_usuario = document.getElementById('tipo_usuario').value;
+        let login = document.getElementById('login').value;
+        let senha = document.getElementById('senha').value;
+
+        window.open(`back_cadastros.php?nome_usuario=${nome_usuario}&&cpf=${cpf}&&data_nascimento=${data_nascimento}
+        &&telefone=${telefone}&&endereco=${endereco}&&num_casa=${num_casa}&&bairro=${bairro}&&fk_id_tb_cidades=${fk_id_tb_cidades}
+        &&tipo_usuario=${tipo_usuario}&&login=${login}&&senha=${senha}`);
+
+    }
+</script>
--- verificacao.php
+++ verificacao.php
@@ -3,7 +3,7 @@
 <?php
 function cadastro($conexao, $login, $senha)
 {
-    $dados = "SELECT tipo_usuario FROM tb_usuarios WHERE login = '$login' AND senha = '$senha'";
+    $dados = "SELECT tipo_usuario, nome_usuario FROM tb_usuarios WHERE login = '$login' AND senha = '$senha'";
     $sql_conexao = $conexao->query($dados) or die($conexao->error);
 
     return $sql_conexao;
@@ -16,10 +16,13 @@ $cadastro_usuario = cadastro($conexao, $login, $senha);
 
 foreach ($cadastro_usuario as $usuario) {
     $tipoUsuario = $usuario['tipo_usuario'];
+    $nome_usuario = $usuario['nome_usuario'];
+    echo $nome_usuario;
 
     if ($tipoUsuario) {
-        header('Location: pagina_inicio.php?tipo_usuario=' . $tipoUsuario);
+        header('Location: pagina_inicio.php?tipo_usuario=' . $tipoUsuario . '&nome=' . $nome_usuario);
+    } else {
+        echo 'nome e senha incorretos!!';
     }
-
 }
 ?>
