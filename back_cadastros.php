c4be13e9630278fd0d8658f4600aa2ee021ce94f
marcelogattermann <marcelo_gattermann@hotmail.com>
2024-02-22 19:57:21 -0300

    22-02

--- back_cadastros.php
+++ back_cadastros.php
@@ -1,1 +1,39 @@
-<?php ?>
+<?php include 'conexao_aula.php'; ?>
+
+<?php
+function cadastro($conexao, $nome_usuario, $cpf, $data_nascimento, $telefone, $endereco, $num_casa, $bairro, $fk_id_cidades, $tipo_usuario, $login, $senha)
+{
+    $dados = "INSERT INTO tb_usuarios (nome_usuario,cpf,data_nascimento,telefone,endereço,num_casa,bairro,fk_id_cidades,ativo,tipo_usuario,login,senha)
+    VALUES ('$nome_usuario', '$cpf','$data_nascimento','$telefone','$endereco', '$num_casa', '$bairro', '$fk_id_cidades', '1', '$tipo_usuario','$login','$senha' )";
+    $sql_conexao = $conexao->query($dados) or die($conexao->error);
+
+    return $sql_conexao;
+}
+
+
+
+$nome_usuario = $_GET["nome_usuario"];
+$cpf = $_GET["cpf"];
+$data_nascimento = $_GET["data_nascimento"];
+$telefone = $_GET["telefone"];
+$endereco = $_GET["endereco"];
+$num_casa = $_GET["num_casa"];
+$bairro = $_GET["bairro"];
+$fk_id_cidades = $_GET["fk_id_tb_cidades"];
+$tipo_usuario = $_GET["tipo_usuario"];
+$login = $_GET["login"];
+$senha = $_GET["senha"];
+
+echo $nome_usuario . '<br>' . $cpf . '<br>' . $data_nascimento . '<br>' . $telefone . '<br>' . $endereco . '<br>' . $num_casa . '<br>' . $bairro
+    . '<br>' . $fk_id_tb_cidades . '<br>' . $tipo_usuario . '<br>' . $login . '<br>' . $senha;
+
+cadastro($conexao, $nome_usuario, $cpf, $data_nascimento, $telefone, $endereco, $num_casa, $bairro, $fk_id_cidades, $tipo_usuario, $login, $senha);
+
+
+
+
+
+
+
+
+?>
---
+++ relatorio_cliente.php
@@ -0,0 +1,110 @@
+<?php include 'conexao_aula.php'; ?>
+
+<?php
+
+function relatorioCliente($conexao)
+{
+    $dados = "SELECT * FROM tb_usuarios";
+    $sql_conexao = $conexao->query($dados) or die($conexao->error);
+    return $sql_conexao;
+
+}
+
+$dadosCliente = relatorioCliente($conexao);
+
+function cidades($conexao, $id_cidade)
+{
+    $dados = "SELECT nome_cidade FROM tb_cidades WHERE id = '$id_cidade'";
+    $sql_conexao = $conexao->query($dados) or die($conexao->error);
+    return $sql_conexao;
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <table class="table table-success table-striped border-default">
+        <thead>
+            <td>id</td>
+            <td>nome_usuario</td>
+            <td>cpf</td>
+            <td>data_nascimento</td>
+            <td>telefone</td>
+            <td>endereço</td>
+            <td>num_casa</td>
+            <td>bairro</td>
+            <td>fk_id_cidades</td>
+            <td>ativo</td>
+            <td>created_at</td>
+            <td>disable_at</td>
+            <td>tipo_usuario</td>
+            <td>login</td>
+            <td>senha</td>
+        </thead>
+        <tbody>
+            <?php foreach ($dadosCliente as $usuarios) { ?>
+                <tr>
+                    <td>
+                        <?php echo $usuarios['id']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['nome_usuario']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['cpf']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['data_nascimento']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['telefone']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['endereço']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['num_casa']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['bairro']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['fk_id_cidades']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['ativo']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['created_at']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['disable_at']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['tipo_usuario']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['login']; ?>
+                    </td>
+                    <td>
+                        <?php echo $usuarios['senha']; ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
